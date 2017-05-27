bd216119b9fa1fb16930aee82433714caaeb8bd9
Enrique Castañeda <mrstinky64@gmail.com>
2017-05-27 17:04:25 -0600

    agregamos validaciones a los datos de entrada y los agregamos a la base de dtos

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateMessageRequest;
 use App\Message;
 use Illuminate\Http\Request;
 
@@ -14,5 +15,14 @@ class MessagesController extends Controller
         return view('messages.show',[
             'message' => $message
         ]);
+    }
+
+    public function create(CreateMessageRequest $request){
+        $message= Message::create([
+            'content'=>$request->input('message'),
+            'image'=>'http://lorempixel.com/600/338?'.mt_rand(0,1000)
+        ]);
+
+        return redirect('/messages/'.$message->id);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,19 @@
     </nav>
 </div>
 <div class="row">
+    <form action="/messages/create" method="post">
+        <div class="form-group @if($errors->has('message')) has-danger @endif">
+            {{csrf_field()}}
+            <input type="text" name="message" class="form-control" placeholder="Que estas pensando?">
+            @if($errors->has('message'))
+                @foreach($errors->get('message') as $error)
+                    <div class="form-control-feedback">{{$error}}</div>
+                @endforeach
+            @endif
+        </div>
+    </form>
+</div>
+<div class="row">
     @forelse($messages as $message)
         <div class="col-6">
             <img class="img-thumbnail" src="{{$message->image}}">
