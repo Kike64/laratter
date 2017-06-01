80b1d24e9ddd4c320e575ad6ddb058254cbd3ebd
Enrique Castañeda <mrstinky64@gmail.com>
2017-05-31 21:10:49 -0600

    se creo una relacion para poder seguirse entre usuarios

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,10 +2,16 @@
 
 @section('content')
     <hi>{{$user->name}}</hi>
-
+    <form action="/{{$user->username}}/follow" method="post">
+        {{csrf_field()}}
+        @if(session('success'))
+            <span class="text-success">{{session('success')}}</span>
+        @endif
+        <button class="btn btn-primary">Follow</button>
+    </form>
     <div class="row">
-    @foreach($user->messages as $message)
-        <div class="col-6">@include('messages.message')</div>
-    @endforeach
+        @foreach($user->messages as $message)
+            <div class="col-6">@include('messages.message')</div>
+        @endforeach
     </div>
 @endsection
