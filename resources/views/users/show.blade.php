252af6d4b31689a68c2cf89ab484382e1b98e38b
Enrique Castañeda <mrstinky64@gmail.com>
2017-05-31 21:42:48 -0600

    se agregaron links para mostrar seguidos y seguidores

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,13 +2,30 @@
 
 @section('content')
     <hi>{{$user->name}}</hi>
-    <form action="/{{$user->username}}/follow" method="post">
-        {{csrf_field()}}
-        @if(session('success'))
-            <span class="text-success">{{session('success')}}</span>
+    <div>
+    <a href="/{{$user->username}}/follows" class="btn btn-link">Sigue a <span class="badge-default">{{$user->follows->count()}}</span></a>
+    <a href="/{{$user->username}}/followers" class="btn btn-link">seguidores <span class="badge-default">{{$user->followers->count()}}</span></a>
+    </div>
+
+    @if(Auth::check())
+        @if(Auth::user()->isFollowing($user))
+            <form action="/{{$user->username}}/unfollow" method="post">
+                {{csrf_field()}}
+                @if(session('success'))
+                    <span class="text-success">{{session('success')}}</span>
+                @endif
+                <button class="btn btn-danger">Dejar de seguir</button>
+            </form>
+        @else
+            <form action="/{{$user->username}}/follow" method="post">
+                {{csrf_field()}}
+                @if(session('success'))
+                    <span class="text-success">{{session('success')}}</span>
+                @endif
+                <button class="btn btn-primary">Follow</button>
+            </form>
         @endif
-        <button class="btn btn-primary">Follow</button>
-    </form>
+    @endif
     <div class="row">
         @foreach($user->messages as $message)
             <div class="col-6">@include('messages.message')</div>
